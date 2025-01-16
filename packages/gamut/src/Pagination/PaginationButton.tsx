b543fa0d27cbbb9587ab25d7555cda87607a1f7e
John Rood <john.rood@skillsoft.com>
2025-01-16 09:33:22 -0600

    upgrade framer motion

--- packages/gamut/src/Pagination/PaginationButton.tsx
+++ packages/gamut/src/Pagination/PaginationButton.tsx
@@ -31,6 +31,7 @@ export type PaginationButtonProps = ComponentProps<
   StyleProps<typeof paginationStrokeButtonStates> & {
     icon?: ComponentType<GamutIconProps>;
     buttonType?: 'stroke' | 'text';
+    href?: string;
   };
 
 export const PaginationButton = forwardRef<
